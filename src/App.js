caa5d116bd033e3f60c777272de44177f70f0857
JunKyus <kuk941025@gmail.com>
2018-12-29 20:37:11 +0900

    delete on click

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import Todos from './Todos'
 
 class App extends Component {
   state = {
-      todo: [
+      todos: [
         {content: 'buying milk', id: 1},
         {content: 'play guitar', id: 2}
       ]
   }
+  deleteTodo = (id) => {
+    
+      const todos = this.state.todos.filter(todo =>{
+        return todo.id !== id
+      });
+      this.setState({
+        todos
+      });
 
+  }
   render() {
     return (
       <div className="todo-app container">
         <h1 className = "center blue-text text-darken-2">Todo List</h1>
-        <Todos todos={this.state.todo}/>
+        <Todos todos={this.state.todos} deleteTodo = {this.deleteTodo}/>
       </div>
     );
   }
--- src/Todos.js
+++ src/Todos.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
-const Todos = ({todos}) => {
-    const todoList = todos.map(todo => {
-        return (
-            <a href=" " className = "collection-item hoverable" key={todo.id}>
-                <span>{todo.content}</span>
-            </a>
-        )
-    }) 
+const Todos = ({todos, deleteTodo}) => {
+    
+    const todoList = todos.length ? (
+        todos.map (todo => {
+            return (
+                <div className="collection-item" key={todo.id}>
+                    <span onClick={() => {deleteTodo(todo.id)}}>{todo.content}</span>
+                </div>
+            )
+        })
+    ) : (
+        <p className="center">No todo's left</p>
+    );
     return (
         <div className = "todos collection">
             {todoList}
